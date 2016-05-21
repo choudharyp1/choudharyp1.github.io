6990fc3cb862f3d0e2545322eef8f1cd6216436a
Pratik Choudhary <Pratik Choudhary>
2016-05-22 02:43:09 +0800

    new design

--- Jquery files/home.js
+++ Jquery files/home.js
@@ -1,81 +1,29 @@
-var main = function (){
-	$(".jumbotron").hide();
-	$("nav").hide();
-	$(".mission").hide();
-	$(".gallery").hide();
-	$(".video").hide();
-	$(".Ending").hide();
+$(document).ready(function(){
 	
-	$("#logo").click(function () {
-		$(this).animate({
-			height: "300px",
-			marginTop: "70px"
-		});
-		$("nav")
-			.css('opacity', 0)
-			.slideDown('slow')
-			.animate(
-				{ opacity: 1},
-				{ queue: false, duration: 'slow' }
-			);
-			
-		setTimeout(function (){
-			$(".mission").fadeIn(1000);
-			$(".gallery").fadeIn();
-			$(".video").fadeIn();
-			$(".Ending").fadeIn();
-		}, 1000);
-		
-	});
 	
-	var a = $("nav").offset().top;
 	
-	$(document).scroll(function () {
-		if ($(this).scrollTop() - 50 > a){
-			$("nav").addClass('fixed');
-		} else{
-			$("nav").removeClass('fixed');
-		}
-	});
 	
-	$('.arrow-next').click(function() {
-    var currentSlide = $('.active-slide');
-    var nextSlide = currentSlide.next();
+	
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-    var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-    if(nextSlide.length === 0) {
-      nextSlide = $('.slide').first();
-      nextDot = $('.dot').first();
-    }
-    
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    nextSlide.fadeIn(600).addClass('active-slide');
+    // Store hash
+    var hash = this.hash;
 
-    currentDot.removeClass('active-dot');
-    nextDot.addClass('active-dot');
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
   });
-
-
-  $('.arrow-prev').click(function() {
-    var currentSlide = $('.active-slide');
-    var prevSlide = currentSlide.prev();
-
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
-
-    if(prevSlide.length === 0) {
-      prevSlide = $('.slide').last();
-      prevDot = $('.dot').last();
-    }
-    
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    prevSlide.fadeIn(600).addClass('active-slide');
-
-    currentDot.removeClass('active-dot');
-    prevDot.addClass('active-dot');
-  });
-}
-
-$(document).ready(main);
+});
