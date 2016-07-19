ff4d4e0d44d2a28fa245d8ff0b1d263608469dfb
Pratik Choudhary <Pratik Choudhary>
2016-07-19 22:10:03 +0800

    resume commit

--- script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		console.log("This is not a mobile friendly site. Please use a computer.");
-	}
-});
